6b43e1bd98af232b0344a333190753cb542299fc
Jarno Rantaharju <j.m.o.rantaharju@swansea.ac.uk>
2019-06-11 15:02:42 +0100

    Set second test result and indentation

--- _includes/code/poisson_test.c
+++ _includes/code/poisson_test.c
@@ -1,43 +1,43 @@
 >>int main(int argc, char** argv) {
 >>
->>  int i, j;
->>  float u[MAX+2][MAX+2], unew[MAX+2][MAX+2], rho[MAX+2][MAX+2];
->>  float h, hsq;
->>  double unorm;
+>>   int i, j;
+>>   float u[MAX+2][MAX+2], unew[MAX+2][MAX+2], rho[MAX+2][MAX+2];
+>>   float h, hsq;
+>>   double unorm;
+>> 
+>>   /* Set up parameters */
+>>   h = 0.1;
+>> 
+>>   /* Run Setup */
+>>   hsq = h*h;
+>> 
+>>   // Initialise the u and rho field to 0 
+>>   for( int j=0; j <= MAX+1; j++ ){
+>>      for( int i=0; i <= MAX+1; i++ ) {
+>>         u[j][i] = 0.0;
+>>         rho[j][i] = 0.0;
+>>      }
+>>   }
 >>
->>  /* Set up parameters */
->>  h = 0.1;
->>
->>  /* Run Setup */
->>  hsq = h*h;
->>
->>  // Initialise the u and rho field to 0 
->>  for( int j=0; j <= MAX+1; j++ ){
->>     for( int i=0; i <= MAX+1; i++ ) {
->>        u[j][i] = 0.0;
->>        rho[j][i] = 0.0;
->>     }
->>  }
->>
->>  // Test a configuration with u=10 at x=1 and y=1
->>  u[1][1] = 10;
->>
->>  // Run a single iteration first
->>  unorm = poisson_step( u, unew, rho, hsq );
->>
->>  if( unorm != 112.5 ){
->>    print("TEST FAILED after 1 iteration");
->>  } else {
->>    print("TEST SUCCEEDED after 1 iteration");
->>  }
->>
->>  for( i=1; i<10; i++)
->>    unorm = poisson_step( u, unew, rho, hsq );
->>
->>  if( unorm != 001950 ){
->>    print("TEST FAILED after 10 iteration");
->>  } else {
->>    print("TEST SUCCEEDED after 10 iteration");
->>  }
+>>   // Test a configuration with u=10 at x=1 and y=1
+>>   u[1][1] = 10;
+>> 
+>>   // Run a single iteration first
+>>   unorm = poisson_step( u, unew, rho, hsq );
+>> 
+>>   if( unorm == 112.5 ){
+>>     printf("TEST SUCCEEDED after 1 iteration\n");
+>>   } else {
+>>     printf("TEST FAILED after 1 iteration\n");
+>>   }
+>> 
+>>   for( i=1; i<10; i++)
+>>     unorm = poisson_step( u, unew, rho, hsq );
+>> 
+>>   if( fabs(unorm - 0.208634816) < 1e-6 ){
+>>     printf("TEST SUCCEEDED after 10 iteration\n");
+>>   } else {
+>>     printf("TEST FAILED after 10 iteration\n");
+>>   }
 >>
 >>}
