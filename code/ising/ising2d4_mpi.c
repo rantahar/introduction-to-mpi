b86155752d2b2f8f820913ac1f191eaa711062e5
Jarno Rantaharju <j.m.o.rantaharju@swansea.ac.uk>
2019-06-19 15:10:31 +0100

    Comments and reformatting to the C mpi version of the ising code

--- code/ising/ising2d4_mpi.c
+++ code/ising/ising2d4_mpi.c
@@ -11,180 +11,193 @@
 #define N1 160
 #define N2 320
 
-#define MAX N1*N2*2
-#define MAXd2 N1*N2
+#define VOLUME N1*N2*2
+#define VOLUMEd2 N1*N2
 
 /*  2d Ising model using Metropolis update algorithm
     periodic boundary condition for x- and y-direction
     version 3 : checker board partitioned
     MPI version
-    assumes that 1-D ring topology in the y direction and no of nodes is
-    a divisor of N2 and N2/numtask is an even integer for convenience */
+    assumes that 1-D ring topology in the y direction and no of ranks is
+    a divisor of N2 and N2/n_ranks is an even integer for convenience */
 
-main(int argc, char** argv) {
+int main(int argc, char** argv) {
 
   short int seed[3];
-  int n,i,j,ij,i2,is,ii,ieo,ioe,itag,iup[MAX*2],idn[MAX*2],iter;
-  int node,numtask,nextup,nextdn,iroot,subN2,subMAX,subMAXd2;
-  float beta,energy0,energy,deltae,env;
-  float s[MAX],stmp,sendbuf[N1],recvbuf[N1];
+  int n,i,j,is,ii,itag,iter;
+  int xup[VOLUME], yup[VOLUME], xdn[VOLUME], ydn[VOLUME];
+  int rank, n_ranks, nextup, nextdn, iroot, subN2, subVOLUME, subVOLUMEd2;
+  float beta, new_energy, energy_now, deltae;
+  float s[VOLUME], stmp, sendbuf[N1], recvbuf[N1];
 
   double esumt,magt,esum,mag,esumsub,magsub;
 
   MPI_Status status;
   MPI_Request request;
 
-  FILE *fp, *fopen();
-
+  /* Initialize MPI and set rank parameters */
   MPI_Init(&argc,&argv);
-  MPI_Comm_rank(MPI_COMM_WORLD,&node);
-  MPI_Comm_size(MPI_COMM_WORLD,&numtask);
-
-  fp = fopen("fort.12","r");
-  fscanf(fp,"%f %d",&beta,&iter);
-
-    /*  printf("beta = "); scanf("%f",&beta);
-	printf("no. of iter = "); scanf("%d",&iter); */
-
-  seed[0]=13+node; seed[1]=35+node; seed[2]=17+node;
-
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  MPI_Comm_size(MPI_COMM_WORLD,&n_ranks);
   iroot = 0;
-  subN2 = N2/numtask;
-  subMAX = MAX/numtask;
-  subMAXd2 = subMAX/2;
+  subN2 = N2/n_ranks;
+  subVOLUME = VOLUME/n_ranks;
+  subVOLUMEd2 = subVOLUME/2;
+  nextup = (rank+1)%n_ranks; nextdn = (rank-1+n_ranks)%n_ranks;
 
-  nextup = (node+1)%numtask; nextdn = (node-1+numtask)%numtask;
+  /* Read parameters from a file. Beta is the inverse of the temperature. */
+  if(rank == 0){
+    FILE *fp = fopen("parameter","r");
+    fscanf(fp,"beta %f\n", &beta);
+    fscanf(fp,"iter %d\n", &iter);
+    fclose(fp);
+    printf("Beta = %f\n", beta);
+    printf("Iter = %d\n", iter);
+  }
 
-  /*  printf("node = %d, %d, %d \n",node,nextup,nextdn);*/
+  /* Broadcast parameters to all ranks */
+  MPI_Bcast( &beta, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+  MPI_Bcast( &iter, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-  for(i = 0;i < subMAX;i++) {
+  /* Set random seed for erand. Needs to be different for each rank */
+  seed[0]=13+rank; seed[1]=35+rank; seed[2]=17+rank;
+
+  /* Initialize each point randomly */
+  for(i = 0;i < subVOLUME;i++) {
     if (erand48(seed) < 0.5)
       s[i] = 1.0; 
     else
       s[i] = -1.0;
   }
 
-  for(j = 0;j < subN2;j++)
-    for(i = 0;i < N1*2;i++) {
-      ij = i + j;
-      i2 = i/2;
-      is = i2 + j*N1;
-      if(ij == (2*(ij/2))) {
-	iup[is] = ((i+1)/2)%N1 + j*N1 + subMAXd2;
-	iup[is+subMAX] = i2 + ((j+1)%subN2)*N1 + subMAXd2;
-	idn[is] = ((i-1+N1*2)/2)%N1 + j*N1 + subMAXd2;
-	idn[is+subMAX] = i2 + ((j-1+N2)%subN2)*N1 + subMAXd2;
-      }
-      else {
-	iup[is+subMAXd2] = ((i+1)/2)%N1 + j*N1;
-	iup[is+subMAXd2+subMAX] = i2 + ((j+1)%subN2)*N1;
-	idn[is+subMAXd2] = ((i-1+N1*2)/2)%N1 + j*N1;
-	idn[is+subMAXd2+subMAX] = i2 + ((j-1+N2)%subN2)*N1;
-      }
+  /* Create and index of neighbours
+     The sites are partitioned to even an odd,
+     with even sites first in the array */
+  for(j = 0;j < subN2;j++) for(i = 0;i < N1*2;i++) {
+    int ij = i + j;
+    int i2 = i/2;
+    int is = i2 + j*N1;
+    if(ij == (2*(ij/2))) {
+	    xup[is] = ((i+1)/2)%N1 + j*N1 + subVOLUMEd2;
+	    yup[is] = i2 + ((j+1)%subN2)*N1 + subVOLUMEd2;
+	    xdn[is] = ((i-1+N1*2)/2)%N1 + j*N1 + subVOLUMEd2;
+	    ydn[is] = i2 + ((j-1+N2)%subN2)*N1 + subVOLUMEd2;
     }
+    else {
+	    xup[is+subVOLUMEd2] = ((i+1)/2)%N1 + j*N1;
+	    yup[is+subVOLUMEd2] = i2 + ((j+1)%subN2)*N1;
+	    xdn[is+subVOLUMEd2] = ((i-1+N1*2)/2)%N1 + j*N1;
+	    ydn[is+subVOLUMEd2] = i2 + ((j-1+N2)%subN2)*N1;
+    }
+  }
 
-  /*  for(i = 0;i < MAX;i++) printf("i = %d, up-x = %d, dn-x = %d, up-y = %d, dn-y = %d\n",i,iup[i],idn[i],iup[i+MAX],idn[i+MAX]); */
-
+  /* Initialize the measurements */
   esumt = 0.0;
   magt = 0.0;
-  for(n = 0;n < iter;n++) {
 
+  /* Run a number of iterations */
+  for(n = 0;n < iter;n++) {
     esum = 0.0;
     mag = 0.0;
     esumsub = 0.0;
     magsub = 0.0;
 
-    for(ieo = 0;ieo < 2;ieo++) {
-      ioe = 1 - ieo;
-
-      /* j = 0 boundary */
-
+    /* Do for even and odd sites */
+    for(int parity=0; parity<2; parity++) {
+      int other_parity = 1 - parity;
+      
+      /* Communicate and update the j=0 boundary */
       itag = 11;
-      ii = ieo*subMAXd2;
+      ii = parity*subVOLUMEd2;
       for(i = 0;i < N1;i++) {
-	sendbuf[i] = s[i+subMAXd2-N1+ioe*subMAXd2];
+	      sendbuf[i] = s[i+subVOLUMEd2-N1+other_parity*subVOLUMEd2];
       }
       MPI_Irecv(&recvbuf,N1,MPI_FLOAT,nextdn,itag,MPI_COMM_WORLD,&request);
-
       MPI_Send(&sendbuf,N1,MPI_FLOAT,nextup,itag,MPI_COMM_WORLD);
-
       MPI_Wait(&request,&status);
 
-      for(i = 0;i < N1;i++) {
-	env = s[iup[i+ii]]+s[iup[i+ii+subMAX]]
-	     +s[idn[i+ii]]+recvbuf[i];
-	energy0 = -beta*s[i+ii]*env;
-	stmp = -s[i+ii];
-	energy=-beta*stmp*env;
-	deltae = energy0-energy;
-	if(exp(deltae) > erand48(seed)) {
-	  s[i+ii] = stmp;
-	  energy0 = energy;
-	}
-	magsub = magsub + s[i+ii];
-	esumsub = esumsub + energy0;
+      for(i=0; i<N1; i++){
+        int neighbours = s[xup[i+ii]] + s[yup[i+ii]]
+	                     + s[xdn[i+ii]] + recvbuf[i];
+	      stmp = -s[i+ii];
+        energy_now = s[i+ii]*neighbours;
+        new_energy = stmp*neighbours;
+        deltae = energy_now-new_energy;
+
+	      if(exp(-beta*deltae) > erand48(seed)) {
+	        s[i+ii] = stmp;
+	        energy_now = new_energy;
+	      }
+	      magsub = magsub + s[i+ii];
+	      esumsub = esumsub + energy_now;
       }
 
+      /* Update the bulk of the lattice, everything but the boundaries */
       for(j = 1;j < (subN2-1);j++) 
-	for(i = 0;i < N1;i++) {
-	  is = i + N1*j + ii;
-	  env = s[iup[is]]+s[iup[is+subMAX]]
-	       +s[idn[is]]+s[idn[is+subMAX]];
-	  energy0 = -beta*s[is]*env;
-	  stmp = -s[is];
-	  energy=-beta*stmp*env;
-	  deltae = energy0-energy;
-	  if(exp(deltae) > erand48(seed)) {
-	    s[is] = stmp;
-	    energy0 = energy;
-	  }
-	  magsub = magsub + s[is];
-	  esumsub = esumsub + energy0;
-	}
+	      for(i = 0;i < N1;i++) {
+	        is = i + N1*j + ii;
+	        int neighbours = s[xup[i]] + s[yup[i]]
+                         + s[xdn[i]] + s[ydn[i]];
+	        stmp = -s[is];
+          energy_now = s[is]*neighbours;
+          new_energy = stmp*neighbours;
+          deltae = energy_now-new_energy;
+	        if(exp(-beta*deltae) > erand48(seed)) {
+	          s[is] = stmp;
+	          energy_now = new_energy;
+	        }
+	        magsub = magsub + s[is];
+	        esumsub = esumsub + energy_now;
+	      }
       
-      /* j = subN2-1 boundary */
-
+      /* update the j = subN2-1 boundary */
       itag = 22;
       for(i = 0;i < N1;i++) {
-	sendbuf[i] = s[i+ioe*subMAXd2];
+	      sendbuf[i] = s[i+other_parity*subVOLUMEd2];
       }
       MPI_Irecv(&recvbuf,N1,MPI_FLOAT,nextup,itag,MPI_COMM_WORLD,&request);
-
       MPI_Send(&sendbuf,N1,MPI_FLOAT,nextdn,itag,MPI_COMM_WORLD);
-
       MPI_Wait(&request,&status);
 
-      ii = ii+subMAXd2-N1;
+      ii = ii+subVOLUMEd2-N1;
       for(i = 0;i < N1;i++) {
-	env = s[iup[i+ii]]+recvbuf[i]
-	     +s[idn[i+ii]]+s[idn[i+ii+subMAX]];
-	energy0 = -beta*s[i+ii]*env;
-	stmp = -s[i+ii];
-	energy=-beta*stmp*env;
-	deltae = energy0-energy;
-	if(exp(deltae) > erand48(seed)) {
-	  s[i+ii] = stmp;
-	  energy0 = energy;
-	}
-	magsub = magsub + s[i+ii];
-	esumsub = esumsub + energy0;
+	      int neighbours = s[xup[i+ii]] + recvbuf[i]
+	                     + s[xdn[i+ii]] + s[ydn[i+ii]];
+	      stmp = -s[i+ii];
+        energy_now = s[i+ii]*neighbours;
+        new_energy = stmp*neighbours;
+        deltae = energy_now-new_energy;
+	      if(exp(-beta*deltae) > erand48(seed)) {
+	        s[i+ii] = stmp;
+	        energy_now = new_energy;
+	      }
+	      magsub = magsub + s[i+ii];
+	      esumsub = esumsub + energy_now;
       }
+
+      /* Sum the energy and magnetisation over the ranks */
       MPI_Reduce(&esumsub,&esum,1,MPI_DOUBLE,MPI_SUM,iroot,MPI_COMM_WORLD);
       MPI_Reduce(&magsub,&mag,1,MPI_DOUBLE,MPI_SUM,iroot,MPI_COMM_WORLD);
     }
-    if(node == 0) {
-      esum = esum/(MAX);
-      mag = mag/(MAX);
-      printf("average energy = %f, average magnetization = %f\n",esum,mag);
+
+    /* Calculate average measurements and print */
+    if(rank == 0) {
+      esum = esum/(VOLUME);
+      mag = mag/(VOLUME);
       esumt = esumt + esum;
       magt = magt + fabs(mag);
+
+      printf("average energy = %f, average magnetization = %f\n",esum,mag);
     }
   }
+
   esumt = esumt/iter;
   magt = magt/iter;
-  if(node == 0)
-  printf("over whole iterations \n average energy = %f, average magnetization = %f\n", esumt, magt);
+  if(rank == 0){
+    printf("over whole iterations \n average energy = %f, average magnetization = %f\n", esumt, magt);
+  }
 
   MPI_Finalize();
 
+  return 0;
 }
