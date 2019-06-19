440f7faf3ff401d69fb4baec8f20095b51a1d4b1
Jarno Rantaharju <j.m.o.rantaharju@swansea.ac.uk>
2019-06-19 14:24:39 +0100

    Reformatting the Ising model in C

--- code/ising/ising2d4.c
+++ code/ising/ising2d4.c
@@ -7,81 +7,107 @@
 #include <stdlib.h>
 
 #define N1 320
-#define N1d2 160
+#define N1d2 (N1/2)
 #define N2 320
 
-#define MAX N1*N2
-#define MAXd2 N1d2*N2
+#define VOLUME N1*N2
+#define VOLUMEd2 N1d2*N2
 
-/*  2d Ising model using Metropolis update algorithm*/
+/* 2d Ising model using Metropolis update algorithm */
 
-main(int argc, char** argv) {
+int main(int argc, char** argv) {
 
   short int seed[3];
-  int n,i,j,ij,i2,is,iup[MAX*2],idn[MAX*2],iter ;
-  float beta,energy0,energy,deltae,esum,mag,env;
-  float s[MAX],stmp;
+  int n,i,j,iter;
+  int xup[VOLUME], yup[VOLUME], xdn[VOLUME], ydn[VOLUME];
+  float beta,esum,mag;
+  int s[VOLUME];
 
   double esumt, magt;
 
-  printf("beta = "); scanf("%f",&beta);
-  printf("no. of iter = "); scanf("%d",&iter);
+  /* Ask for parameters. Beta is the inverse of the temperature */
+  fp = fopen("parameter","r");
+  fscanf(fp,"beta %f %d",&beta);
+  fscanf(fp,"iter %d",&iter);
+  fclose(fp);
 
-  seed[0]=13;seed[1]=35;  seed[2]=17;
+  /* Set random seed for erand */
+  seed[0]=13; seed[1]=35; seed[2]=17;
 
-  for(i = 0;i < MAX;i++) {
-    if (erand48(seed) < 0.5)
+  /* Initialize each point randomly */
+  for(i = 0;i < VOLUME;i++) {
+    if(erand48(seed) < 0.5){
       s[i] = 1.0; 
-    else
+    }
+    else {
       s[i] = -1.0;
+    }
   }
 
-  for(j = 0;j < N2;j++)
-    for(i = 0;i < N1;i++) {
-      ij = i + j;
-      i2 = i/2;
-      is = i2 + j*N1d2;
-      if(ij == (2*(ij/2))) {
-	iup[is] = ((i+1)/2)%N1d2 + j*N1d2 + MAXd2;
-	iup[is+MAX] = i2 + ((j+1)%N2)*N1d2 + MAXd2;
-	idn[is] = ((i-1+N1)/2)%N1d2 + j*N1d2 + MAXd2;
-	idn[is+MAX] = i2 + ((j-1+N2)%N2)*N1d2 + MAXd2;
-      }
-      else {
-	iup[is+MAXd2] = ((i+1)/2)%N1d2 + j*N1d2;
-	iup[is+MAXd2+MAX] = i2 + ((j+1)%N2)*N1d2;
-	idn[is+MAXd2] = ((i-1+N1)/2)%N1d2 + j*N1d2;
-	idn[is+MAXd2+MAX] = i2 + ((j-1+N2)%N2)*N1d2;
-      }
+  /* Create and index of neighbours
+     The sites are partitioned to even an odd,
+     with even sites first in the array */
+  for( j=0; j<N2; j++) for( i=0; i<N1; i++) {
+    int i2 = i/2;
+    int is = i2 + j*N1d2;
+    if( (i+j)%2 == 0 ) {
+	    xup[is] = ((i+1)/2)%N1d2 + j*N1d2 + VOLUMEd2;
+	    yup[is] = i2 + ((j+1)%N2)*N1d2 + VOLUMEd2;
+	    xdn[is] = ((i-1+N1)/2)%N1d2 + j*N1d2 + VOLUMEd2;
+	    ydn[is] = i2 + ((j-1+N2)%N2)*N1d2 + VOLUMEd2;
     }
+    else {
+	    xup[is+VOLUMEd2] = ((i+1)/2)%N1d2 + j*N1d2;
+	    yup[is+VOLUMEd2] = i2 + ((j+1)%N2)*N1d2;
+	    xdn[is+VOLUMEd2] = ((i-1+N1)/2)%N1d2 + j*N1d2;
+	    ydn[is+VOLUMEd2] = i2 + ((j-1+N2)%N2)*N1d2;
+    }
+  }
 
-  /*  for(i = 0;i < MAX;i++) printf("i = %d, up-x = %d, dn-x = %d, up-y = %d, dn-y = %d\n",i,iup[i],idn[i],iup[i+MAX],idn[i+MAX]); */
-
+  /* Initialize the measurements */
   esumt = 0.0;
   magt = 0.0;
+
+  /* Run a number of iterations */
   for(n = 0;n < iter;n++) {
     esum = 0.0;
     mag = 0.0;
-    for(i = 0;i < MAX;i++) {
-      env = s[iup[i]]+s[iup[i+MAX]]+s[idn[i]]+s[idn[i+MAX]];
-      energy0 = -beta*s[i]*env;
+
+    /* Loop over the lattice and try to flip each atom */
+    for(i = 0;i < VOLUME;i++) {
+      float new_energy, energy_now, deltae;
+      int stmp;
+      int neighbours = s[xup[i]] + s[yup[i]] + s[xdn[i]] + s[ydn[i]];
       stmp = -s[i];
-      energy=-beta*stmp*env;
-      deltae = energy0-energy;
-      if(exp(deltae) > erand48(seed)) {
-	s[i] = stmp;
-	energy0 = energy;
+
+      /* Find the energy before and after the flip */
+      energy_now = s[i]*neighbours;
+      new_energy = stmp*neighbours;
+      deltae = energy_now-new_energy;
+      
+      /* Accept or reject the change */
+      if( exp(-beta*deltae) > erand48(seed) ){
+	      s[i] = stmp;
+	      energy_now = new_energy;
       }
+
+      /* Measure magnetisation and energy */
       mag = mag + s[i];
-      esum = esum + energy0;
+      esum = esum + energy_now;
     }
-    esum = esum/(MAX);
-    mag = mag/(MAX);
-    printf("average energy = %f, average magnetization = %f\n",esum,mag);
+
+    /* Calculate measurements and add to run averages  */
+    esum = esum/(VOLUME);
+    mag = mag/(VOLUME);
     esumt = esumt + esum;
     magt = magt + fabs(mag);
+
+    printf("average energy = %f, average magnetization = %f\n",esum,mag);
   }
+
   esumt = esumt/iter;
   magt = magt/iter;
   printf("over whole iterations \n average energy = %f, average magnetization = %f\n", esumt, magt);
+
+  return 0;
 }
